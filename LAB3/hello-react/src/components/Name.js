54542ec7bb4ccaa41f1368698e350881dc5c7864
Pannaporn001 <pannaporn091141@gmail.com>
2021-11-24 12:26:24 +0700

    ok

---
+++ LAB3/hello-react/src/components/Name.js
@@ -0,0 +1,10 @@
+function MyName() {
+    return (
+    <div className="MyName"> 
+        <div>พรรณพร ไชยสงคราม</div>
+        <div>610112418043</div>
+    </div>
+    );
+}
+
+export default MyName
